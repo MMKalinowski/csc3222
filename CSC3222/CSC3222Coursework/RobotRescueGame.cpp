c5a61b1a26a0baad90739c9616a8d0a5743d3f8c
Maciek Kalinowski (UG) <b6047329@newcastle.ac.uk>
2019-02-01 12:42:13 +0000

    fixed vector deletion issue

--- CSC3222/CSC3222Coursework/RobotRescueGame.cpp
+++ CSC3222/CSC3222Coursework/RobotRescueGame.cpp
@@ -46,7 +46,7 @@ void RobotRescueGame::Update(float dt) {
 	for (auto i = gameObjects.begin(); i != gameObjects.end(); ) {
 		if (!(*i)->UpdateObject(dt)) { //object has said its finished with
 			delete (*i);
-			gameObjects.erase(i);
+			i = gameObjects.erase(i);
 		}
 		else {
 			(*i)->DrawObject(*renderer);
